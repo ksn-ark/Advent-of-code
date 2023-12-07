a07704ca666fadb331ea414d2694b48414b949bd
Nark <sooryarox2004@gmail.com>
2023-12-07 18:59:27 +0200

    day 5 c

---
+++ Day_1-10/Day5/Puzzle1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, World!\n");
+
+    // Adding a pause before exiting
+    printf("Press Enter to exit...");
+    getchar(); // This waits for a key press
+
+    return 0;
+}
