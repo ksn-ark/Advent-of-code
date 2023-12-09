3e86187fdbb109199b0504dc5b7081ab9c7041ff
Nark <sooryarox2004@gmail.com>
2023-12-09 20:55:57 +0200

    pzleinput

--- Day_1-10/Day5/Puzzle1.c
+++ Day_1-10/Day5/Puzzle1.c
@@ -1,12 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-int main()
+char *readFile(char *filePath);
+
+void main()
 {
-    printf("Hello, World!\n");
+    char *filePath = "PuzzleInput.txt";
 
-    // Adding a pause before exiting
-    printf("Press Enter to exit...");
-    getchar(); // This waits for a key press
+    char *input = readFile(filePath);
 
-    return 0;
+    printf("%s", input);
+}
+
+char *readFile(char *filePath)
+{
+
+    FILE *inputFile = fopen(filePath, "r");
+
+    fseek(inputFile, 0, SEEK_END);
+    long inputSize = ftell(inputFile);
+    rewind(inputFile);
+
+    char *outputBuffer = (char *)malloc(inputSize + 1);
+
+    fread(outputBuffer, 1, inputSize, inputFile);
+
+    outputBuffer[inputSize] = '\0';
+
+    fclose(inputFile);
+
+    return outputBuffer;
 }
